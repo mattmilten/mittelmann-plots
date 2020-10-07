3771ef3921fed0f623580a80c62a51b00bbff2e9
Matthias Miltenberger <matthias.miltenberger@gmail.com>
2020-10-07 18:14:24 +0200

    Update make-comp-plots.py

--- make-comp-plots.py
+++ make-comp-plots.py
@@ -219,7 +219,7 @@ def write_bench(url, timelimit, threads=1):
 
     plots = '\n'
     plots += f'## [{stats["title"]} ({stats["date"]})]({url})\n\n'
-    plots += '| choose base solver | score | solved |\n'
+    plots += '| choose base solver for comparison | score | solved |\n'
     plots += '| :--- | ---:  | ---:   |\n'
     for s in sorted(stats['score'].items(), key=lambda x: x[1]):
         s = s[0]
@@ -257,10 +257,8 @@ urls = [('http://plato.asu.edu/ftp/lpsimp.html', 15000, 1),
 with open('docs/index.md', 'w') as index:
     index.write("""Interactive Plotly graphs comparing the results of [Hans Mittelmann's benchmarks](http://plato.asu.edu/bench.html).
     Each solver can be selected to show pairwise running time factors for every other solver in the respective benchmark.
-    These plots should make browsing the results easier. Please let [me](https://github.com/mattmilten) know if you have a question or if there is an error.
-
-    Click the solver in the legend to enable or disable it in the plot.
-
+    These plots should make browsing the results easier. Please let [me](https://github.com/mattmilten) know if you have a question or if there is an error.\n
+    Click the solver in the legend to enable or disable it in the plot.\n
     There is a daily check for new benchamrk results.
     
     """)
