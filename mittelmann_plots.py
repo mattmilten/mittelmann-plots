7ebbb20b87f88bb005393ff28e462cbbf3b0ad5f
Matthias Miltenberger <miltenberger@gurobi.com>
2021-10-27 19:03:09 +0200

    clean up

--- mittelmann_plots.py
+++ mittelmann_plots.py
@@ -366,14 +366,14 @@ def write_bench(url, timelimit, threads=1):
         os.system(f"cat docs/{benchname}-{s}.html >> docs/{benchname}-{storedate}.html")
 
     if oldbench:
-        plots += "\n\n<details><summary>🔽 previous benchmarks 🔽</summary>\n<br>\n\n"
+        plots += "\n\n<details><summary>previous benchmarks 🔽</summary>\n<br>\n\n"
         plots += "<ul>\n"
         for ob in oldbench:
             filename = os.path.basename(ob)
             date = filename.lstrip(f"{benchname}-").rstrip(".html").replace("-", " ")
             plots += f'<li><a href="/{filename}">{date}</a></li>\n'
 
-    plots += "\n</ul>\n</details><br>\n\n---\n\n"
+    plots += "</ul>\n</details>\n---\n\n"
 
     return plots
 
