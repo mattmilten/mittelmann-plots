07777c7745d7e172fe8fc23e3968db480add3085
Matthias Miltenberger <miltenberger@gurobi.com>
2021-10-27 19:12:55 +0200

    final cosmetic change

--- mittelmann_plots.py
+++ mittelmann_plots.py
@@ -373,7 +373,7 @@ def write_bench(url, timelimit, threads=1):
             date = filename.lstrip(f"{benchname}-").rstrip(".html").replace("-", " ")
             plots += f'<li><a href="/{filename}">{date}</a></li>\n'
 
-    plots += "</ul>\n</details>\n---\n\n"
+    plots += "</ul></details>\n\n---\n\n"
 
     return plots
 
