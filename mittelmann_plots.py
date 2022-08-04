b97ad05a0e841348c20a468c7c841928d75c56b5
Matthias Miltenberger <matthias.miltenberger@gmail.com>
2022-08-04 11:54:52 +0200

    Update mittelmann_plots.py

--- mittelmann_plots.py
+++ mittelmann_plots.py
@@ -626,7 +626,7 @@ Each solver can be selected to show pairwise running time factors for every othe
 These plots should make browsing the results easier.
 The score ([scaled shifted geometric mean](http://plato.asu.edu/ftp/shgeom.html)) is recomputed using the reported solving times.
 We also calculate a "virtual best" or "ensemble" solver that picks the best performance over all solvers for every single problem
-instance. This might reveal show how much potential the individual solvers still have.
+instance. This might reveal how much potential the individual solvers still have.
 [Please let me know](https://github.com/mattmilten/mittelmann-plots/issues/new) if you have a question or if there is an error.\n
     """
         )
